3fcb2d9f92865b3e362649302edf9e4064f8cfbd
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-04-03 12:32:42 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@ export default {
       { vmid: 'og:locale', property: 'og:locale', content: 'en_US' },
       { vmid: 'og:site_name', property: 'og:site_name', content: 'Avatar Maker' },
       { vmid: 'og:type', property: 'og:type', content: 'website' },
-      { vmid: 'og:image', property: 'og:image', content: 'https://github.com/favrora/Avatar-Maker/blob/master/src/assets/img/preview.jpg' },
+      { vmid: 'og:image', property: 'og:image', content: 'https://github.com/bungferry/Avatar-Maker/blob/master/src/assets/img/preview.jpg' },
 
       // Twitter
       { vmid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },

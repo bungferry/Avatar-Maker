169b23ac5e168efaf0313a7a6c2a91293fb4da3f
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-04-03 12:45:32 +0700

    Update githubIcon.vue

--- src/components/layout/githubIcon.vue
+++ src/components/layout/githubIcon.vue
@@ -2,7 +2,7 @@
 
 <div id="GithubIcon">
 
-  <a target="_blank" href="https://github.com/favrora/Avatar-Maker">
+  <a target="_blank" href="https://www.facebook.com/bungferry.wa">
     <img :src="require('../../assets/img/github.png')">
   </a>
 
